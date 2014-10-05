e56cf37f1dcf6eb2295d78fbd507a02724e208ae
Michael Bishoff <bishoff1@umbc.edu>
2014-10-05 07:18:45 -0400

    Added response Data

--- textMessage.php
+++ textMessage.php
@@ -1,7 +1,10 @@
 <?php
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    
+    // $_REQUEST['Body'];
+
 ?>
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message>There's food at Locker 6: 84 Massachusetts Ave, Cambridge, MA 02139 (Outside of LaVerde's Market)</Message>
 </Response>

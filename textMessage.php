bae66a30fcba3dc13325713cc5612dca3916d754
Michael Bishoff <bishoff1@umbc.edu>
2014-10-05 09:54:36 -0400

    Deleted my two lines

--- textMessage.php
+++ textMessage.php
@@ -1,6 +1,6 @@
 <?php
     header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    echo("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
     
     if (strpos(strtolower($_REQUEST['Body']),'food') !== false) {
         sendText();
@@ -16,4 +16,29 @@
         
     }
 
+/*
+    $query = new ParseQuery("GameScore");
+    $query->equalTo("playerName", "Dan Stemkoski");
+    $results = $query->find();
+    echo "Successfully retrieved " . count($results) . " scores.");
+    // Do something with the returned ParseObject values
+    for ($i = 0; $i < count($results); $i++) { 
+      $object = $results[$i];
+      echo $object->getObjectId() . ' - ' . $object->get('playerName'));
+    }
+*/
+    $query = new ParseQuery("Messages");
+    $query->select("foodItem", "address");
+    $results = $query->find();
+    // each of results will only have the selected fields available.
+    for ($i = 0; $i < count($results); $i++) { 
+        $object = $results[$i];
+        if ($object->get('address') == "_______"){
+            // Add $object->get('foodItem');  to the parachuttes
+        }
+        
+        //echo $object->getObjectId() . ' - ' . $object->get('playerName'));
+    }
+
 ?>
+

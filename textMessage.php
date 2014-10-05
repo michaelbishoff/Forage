38af5ee32c09320f34feddaadd7ced6350708a72
Michael Bishoff <bishoff1@umbc.edu>
2014-10-05 07:26:31 -0400

    Messages must say, 'forage' or 'food' to send a message about food

--- textMessage.php
+++ textMessage.php
@@ -2,9 +2,18 @@
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     
-    // $_REQUEST['Body'];
+    if (strpos($_REQUEST['Body'],'food') !== false) {
+        sendText();
+    }
+    else if (strpos($_REQUEST['Body'],'forage') !== false) {
+        sendText();
+    }
+
+    function sendText(){
+        echo("<Response>");
+        echo("<Message>There's food at Locker 6: 84 Massachusetts Ave, Cambridge, MA 02139 (Outside of LaVerde's Market)</Message>");
+    echo("</Response>");
+        
+    }
 
 ?>
-<Response>
-    <Message>There's food at Locker 6: 84 Massachusetts Ave, Cambridge, MA 02139 (Outside of LaVerde's Market)</Message>
-</Response>
